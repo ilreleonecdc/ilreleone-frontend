1f3b78bcbefcd495c36c6f31844d2716f25e1483
renghinoITS <ilreleonecdc@gmail.com>
2025-05-16 01:44:45 +0200

    Refactor code structure for improved readability and maintainability

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from "primeng/card";
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, ButtonModule, CardModule],
+  imports: [CommonModule, ButtonModule, CardModule, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
