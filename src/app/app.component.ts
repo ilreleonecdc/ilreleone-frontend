405c389d7f2e14bb577f3cf3d1d9d95582db1adc
ilreleonecdc <ilreleonecdc@gmail.com>
2025-05-28 16:23:58 +0200

    Add "Coming Soon" component and update Angular configuration for production build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { CoomingSoonComponent } from "./cooming-soon/cooming-soon.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, ButtonModule, NavbarComponent, FooterComponent, CoomingSoonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
